df0b0e090578a510801a6e94bdca6ef76f855072
Umut Akkaya <umutakkaya1996@gmail.com>
2019-09-29 13:33:01 +0300

    fix

--- app/Http/Controllers/Admin/FilesController.php
+++ app/Http/Controllers/Admin/FilesController.php
@@ -218,7 +218,7 @@ class FilesController extends Controller
 
         return response(null)
             ->header('Content-Disposition', 'attachment; filename="' . $filename . '"')
-            ->header('X-Accel-Redirect', "/storage/app/public/$filename")
+            ->header('X-Accel-Redirect', "storage/app/public/$filename")
             ->header('X-Sendfile', base_path("/storage/app/public/$filename"));
     }
 }
