7b77696f7e3dcc3111a92788faf3f977bc823aa7
Umut Akkaya <Xicy@users.noreply.github.com>
2020-03-08 14:57:55 +0300

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -38,7 +38,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password','role_id'
+        'name', 'email', 'password','role_id', 'haveMasternode'
     ];
 
     /**
